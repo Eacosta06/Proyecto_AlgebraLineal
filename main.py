d2e1dad1ed289799e4cd79a9d2376e1f9ef358c7
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2025-03-21 21:17:54 -0400

    Avance de la función makeMatrix

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 Matrix = [()]
 
-def makeMatrix():
+def intComprobacion(digit):
     None
+
+def makeMatrix():
+    for fila in range(4):
+        row = []
+        for digit in range(4):
+            d = input("Ingreseel dígito a"+fila+digit)
+            d = intComprobacion(d)
+            if d != None:
+                None
+            else:
+                None
 
 def userChoices():
     None
 
 def printMatrix():
     None
+
+print("TEXT HERE")
