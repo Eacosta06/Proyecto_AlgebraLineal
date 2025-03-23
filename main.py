2e7e371efa743beb63269e4bbb35b329a2d8d511
Eacosta <esteacosta@Estebans-MacBook-Pro.local>
2025-03-23 15:08:20 -0400

    Se ha añadido una función de verificación de Matriz nula y matriz identidad

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def makeMatrixB():
     for digit in range(4):
         d = None
         while d == None:
-            d = input("Ingreseel dígito a1"+str(digit)+": ")
+            d = input("Ingreseel dígito a1"+str(digit)+": ").strip()
             d = intComprobacion(d)
             if d != None:
                 d=float(d)
@@ -43,7 +43,7 @@ def makeMatrix():
         for digit in range(4):
             d = None
             while d == None:
-                d = input("Ingrese el dígito a"+str(fila)+str(digit)+": ")
+                d = input("Ingrese el dígito a"+str(fila)+str(digit)+": ").strip()
                 d = intComprobacion(d)
                 if d != None:
                     d=float(d)
@@ -54,12 +54,38 @@ def makeMatrix():
     print("Se ha estalecido la matriz A \n ")
     return Matrix
 
+def matrixComprobacion(Matrix, Vector, a):
+    Null = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+    Id = [[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]
+    if Matrix == Null:
+        print("\033[31mEl Sistema ingresado es nulo. Porfavor ingrese un nuevo sistema\n\033[0m")
+        Matrix = makeMatrix()
+        Vector = makeMatrixB()
+    elif Matrix == Id:
+        print("\nLa matriz A ingresada corresponde a la matriz identidad. Se presentan los resultados: ")
+        print("X1 = "+str(Vector[0]))
+        print("X2 = "+str(Vector[1]))
+        print("X3 = "+str(Vector[2]))
+        print("X4 = "+str(Vector[3]))
+        c = input("¿Desea continuar con la matriz identidad? \ny/n: ").strip()
+        if c == "y":
+            a = 0
+        else:
+            if c != "n":
+                print("\033[31mSe ha ingresado un valor no contemplado\n\033[0m")
+            print("\nPorfavor ingrese un nuevo sistema ")
+            Matrix = makeMatrix()
+            Vector = makeMatrixB()
+    else:
+        a = 0
+    return Matrix, Vector, a
+
 def userChoices(Matrix, Vector, a):
     print("\nOpciones para la resolución del Sistema de Ecuaciones: \n")
     print(" 1- Factoriación LU \n 2- Método de Jacobí \n 3- Gauss-Jordan")
     print(" 4- Imprimir Matriz \n 5- Reemplazar Matrices del Sistema \n")
     print(" 6- Salir \n")
-    option = input("Introduzca la opción que desee aplicar: ").strip()
+    option = input("Introduzca la opción que desee aplicar: \n").strip()
     if option == "1":
         LUmethod.FactorizacionLU(Matrix, Vector)
     elif option == "2":
@@ -81,12 +107,15 @@ def userChoices(Matrix, Vector, a):
 def printMatrix(Matrix, Vector):
     for i in range(4):
         for j in range(4):
-            print(str(Matrix[i][j]), end=" ")
+            print(str(Matrix[i][j]), end="  ")
         print("| "+str(Vector[i]))
 
 print("Bienvenido \n ")
 Matrix = makeMatrix()
 Vector = makeMatrixB()
+a = 1
+while a == 1:
+    Matrix, Vector, a = matrixComprobacion(Matrix, Vector, a)
 a = 0
 while a == 0:
     Matrix, Vector, a = userChoices(Matrix, Vector, a)
